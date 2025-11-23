f1ae9ca6d0f256d956bb3bcf94d410b9befc97a5
Dilmini1027 <dilminishyavindi@gmail.com>
2025-11-24 01:20:20 +0530

    added a project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,7 +12,14 @@ export const Projects = () => {
       title: 'Land Acquisition Management System',
       description: 'A full-stack web app to manage land acquisition processes.',
       link: 'https://github.com/sashith06/Land_Aquisition_Management_System.git',
-      image: '../WhatsApp Image 2025-06-14 at 21.18.15_c4db2e7d.jpg',
+      image: './WhatsApp Image 2025-11-24 at 00.45.29_77e09563.jpg',
+      category: 'Web App',
+    },
+    {
+      title: 'BBQ restaurant webapp',
+      description: 'A frontend web app to track orders and manage menu items.',
+      link: 'https://bbq-restaurant-app-49ta.vercel.app/',
+      image: './Screenshot 2025-11-23 231622.png',
       category: 'Web App',
     },
 
